5dbd9f91176561b3d58ca17b2610a8ba4c7cabd6
AlexMarugan <alex.marugan@icloud.com>
2026-09-08 17:44:21 +0200

    Add files via upload
    Cambios visuales y de fecha

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'rondas-v6-print-week-selection';
+const CACHE = 'rondas-v7-print-layout-date-fix';
 const ASSETS = ['./', './index.html', './styles.css', './app.js', './manifest.webmanifest', './icono-Rondas.png'];
 self.addEventListener('install', event => event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS))));
 self.addEventListener('fetch', event => event.respondWith(caches.match(event.request).then(cached => cached || fetch(event.request))));
